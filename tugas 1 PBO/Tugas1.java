233c93a94974dcbd0e34b989fe8b300c34260e5e
Amarhum <114468001+Amarhum@users.noreply.github.com>
2022-09-27 22:38:50 -0700

    Add files via upload

---
+++ tugas 1 PBO/Tugas1.java
@@ -0,0 +1,8 @@
+import javax.swing.*;
+public class Tugas1 {
+    public static void main(String[] args) throws Exception {
+        JOptionPane.showInternalMessageDialog(null, "Belajar JAva\n Sangat Mudah Seklai\nDan Sangat Menyenangkan");
+        JOptionPane.showInputDialog("Anda Sedang Belajar Apa ?");
+        JOptionPane.showInternalMessageDialog(null, "Brlajar pemrograman web sangat mudah");
+    }
+}
